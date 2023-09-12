1938c74378aeef9765016ab4820b7e0fc82d1509
김명식 <rlaaudtlr233@naver.com>
2023-09-12 14:45:09 +0900

    git add .

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
+var data = [1, 2, 3, 4];
 app.get("/", function (req, res) {
-    res.send({ name: "kim", age: 28, friends: [1, 2, 3, 4] });
+    res.send({ cats: app_model_1.Cat });
 });
 app.listen(port, function () {
-    console.log("Example app listening on port " + port);
+    console.log("server is on port " + port);
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import * as express from "express";
+import { Cat, CatType } from "./app.model";
+
 const app: express.Express = express();
 const port: number = 8000;
+
+const data = [1, 2, 3, 4];
+
 // 라우터
-app.get("/", (req, res) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   // res.send로 프론트에 응답을 보내줌
-  res.send({ name: "kim", age: 28, friends: [1, 2, 3, 4] });
+  res.send({ cats: Cat });
 });
+
 // 서버를 실행시킴 즉, 요청이 오는걸 기다리는 상태
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`server is on port ${port}`);
 });
